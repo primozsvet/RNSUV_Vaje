091769d07d9541fa065b1bd92f1fd4c065ef773c
primozsvet <36708142+primozsvet@users.noreply.github.com>
2025-10-22 17:42:51 +0200

    Pripravljena glava in noga aplikacije

--- Contacts-App/src/app/app-module.ts
+++ Contacts-App/src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Footer
   ],
   imports: [
     BrowserModule,
