fbbe6f54a7e98f1e77197fe67aee5732d2b29d11
primozsvet <36708142+primozsvet@users.noreply.github.com>
2025-10-29 17:23:55 +0100

    Dodan osnovni data binding

--- Contacts-App/src/app/features/contacts/pages/contacts-overview/contacts-overview.ts
+++ Contacts-App/src/app/features/contacts/pages/contacts-overview/contacts-overview.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacts-overview',
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   templateUrl: './contacts-overview.html',
   styleUrl: './contacts-overview.css'
 })
-export class ContactsOverview {
+export class ContactsOverview implements OnInit {
+
+  public name: string = '';
+  public surname: string = '';
+  public phone: string = '';
+  public email: string = '';
+  public company: string = '';
+
+  public name2: string = '';
+  public surname2: string = '';
+  public phone2: string = '';
+  public email2: string = '';
+  public company2: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.name = 'Ana';
+    this.surname = 'Novak';
+    this.phone = '+38640123456';
+    this.email = 'ana.novak@email.si';
+    this.company = 'Mercator';
+
+    this.name2 = 'Bojan';
+    this.surname2 = 'Kralj';
+    this.phone2 = '+38640123456';
+    this.email2 = 'bojan.kralj@email.si';
+    this.company2 = 'Mercator';
+  }
 
 }
