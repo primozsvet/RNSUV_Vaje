6c49e9eb837eec9dc52140650ef78168fe5d1b3f
primozsvet <36708142+primozsvet@users.noreply.github.com>
2025-11-05 17:29:37 +0100

    Dodano onemogocanje gumba

--- Contacts-App/src/app/features/authentication/pages/signup/signup.ts
+++ Contacts-App/src/app/features/authentication/pages/signup/signup.ts
@@ -8,16 +8,23 @@ import { User } from '../../../../shared/classes/user';
   styleUrl: './signup.css'
 })
 export class Signup {
+  
   public user: User;
+  public confirmed: boolean = false;
 
   constructor() {
     this.user = new User ('','','','');
   }
 
   public onSubmit(): void {
-    let tekst = `Registracija uporabnika:\nIme: ${this.user.name}\nPriimek: ${this.user.surname}\nEmail: ${this.user.email}\nGeslo: ${this.user.pass}`;
-    alert(tekst);
-    console.log(tekst);
+    if(this.confirmed && this.user.name && this.user.surname && this.user.email && this.user.pass) {
+      let tekst = `Registracija uporabnika:\nIme: ${this.user.name}\nPriimek: ${this.user.surname}\nEmail: ${this.user.email}\nGeslo: ${this.user.pass}`;
+      alert(tekst);
+      console.log(tekst);
+    } else {
+      alert("Obrazec ni ustrezno izpolnjen!");
+      console.log("Obrazec ni ustrezno izpolnjen!");
+    }
 
     //console.log('Registrirani uporabnik:', this.user.name);
   }
