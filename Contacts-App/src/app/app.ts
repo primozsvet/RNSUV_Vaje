0d585dd647dc6d637fce7e8a99ee4ff67f716267
primozsvet <36708142+primozsvet@users.noreply.github.com>
2025-11-05 19:31:49 +0100

    Dodan obrazec za urejanje kontakta

--- Contacts-App/src/app/app.ts
+++ Contacts-App/src/app/app.ts
@@ -11,6 +11,6 @@ export class App {
 
   public loggedIn = true;
   public isLoginForm = true;
-  public contactViewMode: string = 'new';
+  public contactViewMode: string = 'edit';
 
 }
--- Contacts-App/src/app/features/contacts/pages/contacts-edit/contacts-edit.ts
+++ Contacts-App/src/app/features/contacts/pages/contacts-edit/contacts-edit.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../../../shared/classes/contact';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts-edit',
@@ -7,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './contacts-edit.css'
 })
 export class ContactsEdit {
+  public contact: Contact;
 
+  constructor() {
+    this.contact = new Contact('Ana', 'Novak', '041 123 456', 'ana.novak@email.si', 'Mercator');
+  }
+
+  public onSubmit(contactForm: NgForm): void {
+
+    if (contactForm.valid) {
+
+      let text = "";
+
+      if (this.contact.company) {
+        text = `Urejanje kontakta:\nIme: ${this.contact.name}\nPriimek: ${this.contact.surname}\nTelefon: ${this.contact.phone}\nEmail: ${this.contact.email}\nPodjetje: ${this.contact.company}`;
+      } else {
+        text = `Urejanje kontakta:\nIme: ${this.contact.name}\nPriimek: ${this.contact.surname}\nTelefon: ${this.contact.phone}\nEmail: ${this.contact.email}`;
+      }
+      alert(text);
+      console.log(text);
+    } else {
+      alert("Prosimo, izpolnite vse zahtevane podatke za urejanje kontakta.");
+      console.log("Prosimo, izpolnite vse zahtevane podatke za urejanje kontakta.");
+    }
+  }
 }
