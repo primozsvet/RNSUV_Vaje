039f9fdd36368735b1bf4fd4ba8f64fb605b0f86
primozsvet <36708142+primozsvet@users.noreply.github.com>
2025-12-05 16:51:26 +0100

    Dodan zacetek usmerjanja

--- Contacts-App/src/app/app-routing-module.ts
+++ Contacts-App/src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Komponente
+import { Login } from './features/authentication/pages/login/login';
+import { Signup } from './features/authentication/pages/signup/signup';
+import { ContactsOverview } from './features/contacts/pages/contacts-overview/contacts-overview';
+import { ContactsNew } from './features/contacts/pages/contacts-new/contacts-new';
+
+const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'contacts', component: ContactsOverview },
+  { path: 'contacts/new', component: ContactsNew }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
